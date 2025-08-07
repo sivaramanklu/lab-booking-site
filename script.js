ed61052cf307ca92ff110220ef8870ebf7a9361b
sivaramanklu <s.sivaraman@klu.ac.in>
2025-08-07 12:26:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
+document.getElementById("loginForm").addEventListener("submit", async function (e) {
   e.preventDefault();
 
   const facultyId = document.getElementById("facultyId").value;
   const password = document.getElementById("password").value;
 
-  // In next steps, we'll fetch this from backend
-  if (facultyId === "admin" && password === "admin") {
+  const res = await fetch("http://127.0.0.1:5000/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ faculty_id: facultyId, password }),
+  });
+
+  const data = await res.json();
+
+  if (data.success) {
+    localStorage.setItem("user", JSON.stringify(data));
     window.location.href = "dashboard.html";
   } else {
-    document.getElementById("loginMsg").textContent = "Invalid credentials.";
+    document.getElementById("loginMsg").textContent = data.message;
   }
 });
